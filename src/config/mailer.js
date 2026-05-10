1ae9da88e9c8d6343ec853165a6dbc606fa511df
Esraa Shaban <esraashaban2231@gmail.com>
2026-05-10 10:18:42 +0300

    add Structure

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,12 +1,38 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
+// const sendEmail = async ({ to, subject, html }) => {
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `"Dawaya" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     });
+
+//     console.log("Email sent:", info.messageId);
+//   } catch (error) {
+//     console.error("Email error:", error.message);
+//     throw new Error("Email sending failed");
+//   }
+// };
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
@@ -18,9 +44,11 @@ const sendEmail = async ({ to, subject, html }) => {
     });
 
     console.log("Email sent:", info.messageId);
+    return true;
+
   } catch (error) {
     console.error("Email error:", error.message);
-    throw new Error("Email sending failed");
+    return false;
   }
 };
 
