d9a1e3edf3cb3586bb564b65d81e6cd256d032c1
Esraa Shaban <esraashaban2231@gmail.com>
2026-05-11 11:09:16 +0300

    create endpoint

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-process.on('uncaughtException', (error) => {
-  console.error('Uncaught Exception:', error);
-});
-
-process.on('unhandledRejection', (err, promise) => {
-  console.error(`Unhandled Rejection: ${err.message}`);
-  server.close(() => process.exit(1));
-});
+process.on('unhandeledRejection' , (err,promise)=>{
+    console.log(`Error : ${err.message}`);
+    server.close(()=> process.exit(1)); 
+})
