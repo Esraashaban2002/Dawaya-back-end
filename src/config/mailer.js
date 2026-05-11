a113301bb279256040e53f4ba05911ba862c58c3
Esraa Shaban <esraashaban2231@gmail.com>
2026-05-11 11:06:56 +0300

    create endpoint

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,17 +1,18 @@
 const nodemailer = require("nodemailer");
-
+const EMAIL_USER='esraashaban231@gmail.com'
+const EMAIL_PASS='htsiysivpwjfyyax'
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
   },
 });
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const info = await transporter.sendMail({
-      from: `"Dawaya" <${process.env.EMAIL_USER}>`,
+      from: `"Dawaya" <${EMAIL_USER}>`,
       to,
       subject,
       html,
