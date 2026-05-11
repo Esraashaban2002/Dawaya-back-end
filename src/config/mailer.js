e7dab3189ee23080588a2fecb0d7859ea49cff92
Esraa Shaban <esraashaban2231@gmail.com>
2026-05-11 17:04:34 +0300

    create endpoint

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,27 +1,21 @@
-const nodemailer = require("nodemailer");
+const Brevo = require("@getbrevo/brevo");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-   family: 4, 
-});
-
+const apiInstance = new Brevo.TransactionalEmailsApi();
+apiInstance.setApiKey(
+  Brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"Dawaya" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
+    const sendSmtpEmail = new Brevo.SendSmtpEmail();
+    sendSmtpEmail.subject = subject;
+    sendSmtpEmail.htmlContent = html;
+    sendSmtpEmail.sender = { name: "Dawaya", email: process.env.BREVO_SENDER };
+    sendSmtpEmail.to = [{ email: to }];
 
-    console.log("Email sent:", info.messageId);
+    const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
+    console.log("Email sent:", result.body.messageId);
   } catch (error) {
     console.error("Email error:", error.message);
     throw new Error("Email sending failed");
