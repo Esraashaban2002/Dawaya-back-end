83e037d178cb3fbac63dc5adf3952242fc487b85
Esraa Shaban <esraashaban2231@gmail.com>
2026-05-11 17:17:15 +0300

    create endpoint

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,21 +1,27 @@
-const Brevo = require("@getbrevo/brevo");
+const nodemailer = require("nodemailer");
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(
-  Brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+   family: 4, 
+});
+
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    const sendSmtpEmail = new Brevo.SendSmtpEmail();
-    sendSmtpEmail.subject = subject;
-    sendSmtpEmail.htmlContent = html;
-    sendSmtpEmail.sender = { name: "Dawaya", email: process.env.BREVO_SENDER };
-    sendSmtpEmail.to = [{ email: to }];
+    const info = await transporter.sendMail({
+      from: `"Dawaya" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
 
-    const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    console.log("Email sent:", result.body.messageId);
+    console.log("Email sent:", info.messageId);
   } catch (error) {
     console.error("Email error:", error.message);
     throw new Error("Email sending failed");
@@ -23,44 +29,3 @@ const sendEmail = async ({ to, subject, html }) => {
 };
 
 module.exports = { sendEmail };
-
-
-// const nodemailer = require("nodemailer");
-
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-//   connectionTimeout: 10000,
-//   socketTimeout: 15000,
-//   family: 4,
-// });
-
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.error("SMTP Connection Failed:", error);
-//   } else {
-//     console.log("SMTP Ready ✓");
-//   }
-// });
-
-// const sendEmail = async ({ to, subject, html }) => {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"Dawaya" <${process.env.EMAIL_USER}>`,
-//       to,
-//       subject,
-//       html,
-//     });
-//     console.log("Email sent:", info.messageId);
-//   } catch (error) {
-//     console.error("Email error:", error.message);
-//     throw new Error("Email sending failed");
-//   }
-// };
-
-// module.exports = { sendEmail };
