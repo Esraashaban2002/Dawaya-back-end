f8f9a2253ac1cf0c07e3310d5586cd71d09aeb94
Esraa Shaban <esraashaban2231@gmail.com>
2026-06-07 19:38:04 +0300

    Delete medicine endpoint

--- src/routes/medicineRouter.js
+++ src/routes/medicineRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMedicines, getMedicineById, createMedicine, updateMedicine } = require('../controllers/medicineController');
+const { getAllMedicines, getMedicineById, createMedicine, updateMedicine, deleteMedicine } = require('../controllers/medicineController');
 const { auth, isAdmin } = require('../middlewares/auth');
 
 /**
@@ -166,5 +166,30 @@ router.post('/', createMedicine);
  *         description: Internal server error
  */
 router.put('/:id', updateMedicine);
+
+/**
+ * @swagger
+ * /api/medicines/{id}:
+ *   delete:
+ *     summary: Delete medicine
+ *     tags: [Medicines]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Medicine ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Medicine deleted successfully
+ *       404:
+ *         description: Medicine not found
+ *       500:
+ *         description: Internal server error
+ */
+router.delete('/:id', deleteMedicine);
 
 module.exports = router;
